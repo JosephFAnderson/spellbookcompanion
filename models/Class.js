dcb44d2f4ba0c0e5a2599704454855f2e97df96b
Joseph A <joseph_anderson11@yahoo.com>
2023-03-25 12:41:44 -0400

    Classes table created

---
+++ models/Class.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes }= require('sequelize');
+const sequelize = require('../config/connection');
+
+class Class extends Model {};
+
+Class.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.TEXT,
+        require: true,        
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "classes"
+});
+
+module.exports = Class;
