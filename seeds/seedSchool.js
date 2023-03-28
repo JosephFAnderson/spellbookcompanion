eaa0e5da748134336d40f086876496066a35d927
Joseph A <joseph_anderson11@yahoo.com>
2023-03-28 14:40:56 -0400

    seed data created

---
+++ seeds/seedSchool.js
@@ -0,0 +1,23 @@
+const { School } = require("../models");
+const fs = require("fs");
+
+const seedSchools = async () => {
+    fs.readFile("./seeds/schools.csv", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    
+        const dataArray = data.split('\r\n');
+        seeding(dataArray);
+    })
+    
+}
+
+const seeding = async (array) => {
+    for(let i = 1; i < array.length; i++) {
+        await School.create({name: array[i]});
+    }
+}
+
+module.exports = seedSchools;
